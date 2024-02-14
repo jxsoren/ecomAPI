039cf31f5bed7569606478cde7e84e133633c2e4
jxsoren <jxsoren@gmail.com>
2024-02-13 23:14:20 -0700

    added gorm + POST /items endpoint to create row

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,28 +9,23 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-type item struct {
-	ID          string `json:"id"`
-	Description string `json:"description"`
-	Price       int    `json:"price"`
-	Quantity    int    `json:"quantity"`
-}
+var db *gorm.DB
 
-var items = []item{
-	{ID: "1", Description: "Item 1", Price: 100, Quantity: 10},
-	{ID: "2", Description: "Item 2", Price: 200, Quantity: 20},
-	{ID: "3", Description: "Item 3", Price: 300, Quantity: 30},
-}
-
-func main() {
+func init() {
+	var err error
 
 	// Load Env variabless
-	err := godotenv.Load()
-	if err != nil {
+	envErr := godotenv.Load()
+	if envErr != nil {
 		log.Fatal("Error loading .env")
 	}
+
+	fmt.Println("Env Loaded Successfully!")
 
 	// DB Environment Variables
 	dbUsername := os.Getenv("DB_USER")
@@ -40,22 +34,60 @@ func main() {
 	dbName := os.Getenv("DB_NAME")
 
 	// DB Connection String
-	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s", dbUsername, dbPassword, dbHost, dbName)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUsername, dbPassword, dbHost, dbName)
+	fmt.Println(dsn)
 
 	// Connect to DB using DSN
-	db, err := sql.Open("mysql", dsn)
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Error opening DB: %v", err)
 	}
-	defer db.Close()
 
-	// Verify connection to DB
-	if err := db.Ping(); err != nil {
-		log.Fatalf("Error connecting to DB: %v", err)
+	// Try to get underlying MySQL db
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatalf("Failed to get underlying sql.DB: %v", err)
 	}
 
-	log.Println("Connection to DB is sucessful")
+	// Try to ping DB to verify connection
+	err = sqlDB.Ping()
+	if err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
+	} else {
+		fmt.Println("Successfully connected to the database")
+	}
 
+	// Creates Table (if it doesn't exist)
+	db.AutoMigrate(&ProductItem{})
+	if err != nil {
+		log.Fatalf("Error during migration: %v", err)
+	}
+
+	fmt.Println("Connected to DB")
+}
+
+type ProductItem struct {
+	ID          string `gorm:"primaryKey;type:varchar(100)" json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Price       int    `json:"price"`
+	Quantity    int    `json:"quantity"`
+}
+
+type Item struct {
+	ID          string `json:"id"`
+	Description string `json:"description"`
+	Price       int    `json:"price"`
+	Quantity    int    `json:"quantity"`
+}
+
+var items = []Item{
+	{ID: "1", Description: "Item 1", Price: 100, Quantity: 10},
+	{ID: "2", Description: "Item 2", Price: 200, Quantity: 20},
+	{ID: "3", Description: "Item 3", Price: 300, Quantity: 30},
+}
+
+func main() {
 	// Init Gin Router
 	router := gin.Default()
 
@@ -78,10 +110,17 @@ func main() {
 }
 
 func getItems(c *gin.Context) {
+	var items []Item
+	results := db.Find(&items)
+
+	if results.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": results.Error.Error()})
+	}
+
 	c.IndentedJSON(http.StatusOK, items)
 }
 
-func removeItem(s []item, index int) []item {
+func removeItem(s []Item, index int) []Item {
 	return append(s[:index], s[index+1:]...)
 }
 
@@ -99,24 +138,28 @@ func itemHandler(c *gin.Context) {
 		}
 	case "POST":
 
-		var item item
+		var productItem ProductItem
 
 		// Bind request body & check for errors
-		if err := c.BindJSON(&item); err != nil {
+		if err := c.BindJSON(&productItem); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
 		}
 
-		items = append(items, item)
+		// Insert new item into DB
+		result := db.Create(&productItem)
+		fmt.Println(productItem)
 
-		c.JSON(http.StatusOK, gin.H{
-			"id":          item.ID,
-			"description": item.Description,
-			"price":       item.Price,
-			"quantity":    item.Quantity,
-		})
+		// Throw 500 error if creation fails
+		if result.Error != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, productItem)
 
 	case "PUT":
-		var item item
+		var item Item
 
 		id := c.Param("id")
 
