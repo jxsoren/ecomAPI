190d43fe470927ee590c87a115c401c7baee27a9
jxsoren <jxsoren@gmail.com>
2024-02-17 13:54:29 -0700

    added api versioning via grouping

--- routes/routes.go
+++ routes/routes.go
@@ -9,53 +9,60 @@ import (
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
 
-	// Product Routes
-	productRoutes := router.Group("/products")
+	apiRoutes := router.Group("/api")
+
+	v1Routes := apiRoutes.Group("/v1")
 	{
-		// Base route (get all products)
-		productRoutes.GET("/", controllers.GetItems)
 
-		// Create, Read, Update, Delete products
-		productRoutes.GET("/:id", controllers.ItemHandler)
-		productRoutes.POST("/", controllers.ItemHandler)      // (Admin Only)
-		productRoutes.PUT("/:id", controllers.ItemHandler)    // (Admin Only)
-		productRoutes.DELETE("/:id", controllers.ItemHandler) // (Admin Only)
+		// Root Route
+		router.GET("/", controllers.BaseHandler)
 
-		// Product variants
-		productRoutes.GET("/:id/variants", controllers.BaseHandler)
-		productRoutes.POST("/:id/variants", controllers.BaseHandler)     // (Admin Only)
-		productRoutes.POST("/:id/variants/:id", controllers.BaseHandler) // (Admin Only)
-		productRoutes.POST("/:id/variants/:id", controllers.BaseHandler) // (Admin Only)
+		// Product Routes
+		productRoutes := v1Routes.Group("/products")
+		{
+			// Base route (get all products)
+			productRoutes.GET("/", controllers.GetItems)
 
-		// Product inventory
-		productRoutes.GET("/:id/stock", controllers.BaseHandler)
-		productRoutes.PUT("/:id/stock", controllers.BaseHandler) // (Admin Only)
+			// Create, Read, Update, Delete products
+			productRoutes.GET("/:id", controllers.ItemHandler)
+			productRoutes.POST("/", controllers.ItemHandler)      // (Admin Only)
+			productRoutes.PUT("/:id", controllers.ItemHandler)    // (Admin Only)
+			productRoutes.DELETE("/:id", controllers.ItemHandler) // (Admin Only)
 
-		// Product offers/deals
-		productRoutes.GET("/offers", controllers.BaseHandler)
-		productRoutes.PUT("/:id/offer", controllers.BaseHandler) // (Admin Only)
+			// Product variants
+			productRoutes.GET("/:id/variants", controllers.BaseHandler)
+			productRoutes.POST("/:id/variants", controllers.BaseHandler)     // (Admin Only)
+			productRoutes.POST("/:id/variants/:id", controllers.BaseHandler) // (Admin Only)
+			productRoutes.POST("/:id/variants/:id", controllers.BaseHandler) // (Admin Only)
 
-		// Rating and Reviews
-		productRoutes.GET("/reviews", controllers.BaseHandler)
-		productRoutes.GET("/:id/reviews", controllers.BaseHandler)
-		productRoutes.POST("/:id/reviews", controllers.BaseHandler)
-		productRoutes.PUT("/:id/reviews", controllers.BaseHandler)    // (Admin Only)
-		productRoutes.DELETE("/:id/reviews", controllers.BaseHandler) // (Admin Only)
+			// Product inventory
+			productRoutes.GET("/:id/stock", controllers.BaseHandler)
+			productRoutes.PUT("/:id/stock", controllers.BaseHandler) // (Admin Only)
 
-		// Search/Query products
-		productRoutes.GET("/search", controllers.BaseHandler)
-		productRoutes.GET("/search/category", controllers.BaseHandler)
+			// Product offers/deals
+			productRoutes.GET("/offers", controllers.BaseHandler)
+			productRoutes.PUT("/:id/offer", controllers.BaseHandler) // (Admin Only)
 
-		// Product Recommendations
-		productRoutes.GET("/recommendations", controllers.BaseHandler)
+			// Rating and Reviews
+			productRoutes.GET("/reviews", controllers.BaseHandler)
+			productRoutes.GET("/:id/reviews", controllers.BaseHandler)
+			productRoutes.POST("/:id/reviews", controllers.BaseHandler)
+			productRoutes.PUT("/:id/reviews", controllers.BaseHandler)    // (Admin Only)
+			productRoutes.DELETE("/:id/reviews", controllers.BaseHandler) // (Admin Only)
 
-		// Product Analyitics
-		productRoutes.GET("/:id/analyitics", controllers.BaseHandler) // (Admin Only)
+			// Search/Query products
+			productRoutes.GET("/search", controllers.BaseHandler)
+			productRoutes.GET("/search/category", controllers.BaseHandler)
+
+			// Product Recommendations
+			productRoutes.GET("/recommendations", controllers.BaseHandler)
+
+			// Product Analyitics
+			productRoutes.GET("/:id/analyitics", controllers.BaseHandler) // (Admin Only)
+
+		}
 
 	}
-
-	// Root Route
-	router.GET("/", controllers.BaseHandler)
 
 	return router
 }
