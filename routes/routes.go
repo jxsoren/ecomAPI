bbece6e2cba6f0f7ee1d8298e0dc515502bfab71
jxsoren <jxsoren@gmail.com>
2024-02-20 19:33:33 -0700

    added comments for uncompleted handlers

--- routes/routes.go
+++ routes/routes.go
@@ -35,14 +35,17 @@ func SetupRouter() *gin.Engine {
 			productRoutes.PUT("/:id/variants/:variant_id", products.VariantsHandler)    // (Admin Only)
 			productRoutes.DELETE("/:id/variants/:variant_id", products.VariantsHandler) // (Admin Only)
 
+			// TODO: create handlers for routes
 			// Product inventory
 			productRoutes.GET("/:id/stock")
 			productRoutes.PUT("/:id/stock") // (Admin Only)
 
+			// TODO: create handlers for routes
 			// Product offers/deals
 			productRoutes.GET("/offers")
 			productRoutes.PUT("/:id/offer") // (Admin Only)
 
+			// TODO: create handlers for routes
 			// Rating and Reviews
 			productRoutes.GET("/reviews")
 			productRoutes.GET("/:id/reviews")
@@ -50,35 +53,43 @@ func SetupRouter() *gin.Engine {
 			productRoutes.PUT("/:id/reviews")    // (Admin Only)
 			productRoutes.DELETE("/:id/reviews") // (Admin Only)
 
+			// TODO: create handlers for routes
 			// Search/Query products
 			productRoutes.GET("/search")
 			productRoutes.GET("/search/category")
 
+			// TODO: create handlers for routes
 			// Product Recommendations
 			productRoutes.GET("/recommendations")
 
+			// TODO: create handlers for routes
 			// Product Analyitics
 			productRoutes.GET("/:id/analyitics") // (Admin Only)
 		}
 
 		userRoutes := v1Routes.Group("/users")
 		{
+			// TODO: create handlers for routes
 			// User registration
 			userRoutes.POST("/signup")
 
+			// TODO: create handlers for routes
 			// User authentication
 			userRoutes.POST("/authentication")
 			userRoutes.POST("logout")
 
+			// TODO: create handlers for routes
 			// Profile management
 			userRoutes.GET("/profile")
 			userRoutes.PUT("/profile")
 			userRoutes.DELETE("/profile")
 
+			// TODO: create handlers for routes
 			// Password management
 			userRoutes.POST("/forgot-password")
 			userRoutes.POST("/reset-password")
 
+			// TODO: create handlers for routes
 			// User administration
 			userRoutes.GET("/users")     // (Admin Only)
 			userRoutes.GET("/users/:id") // (Admin Only)
@@ -88,41 +99,52 @@ func SetupRouter() *gin.Engine {
 
 		cartRoutes := v1Routes.Group("/cart")
 		{
+			// TODO: create handlers for routes
 			// View cart
 			cartRoutes.GET("/")
 
+			// TODO: create handlers for routes
 			// Get cart item count
 			cartRoutes.GET("/count")
 
+			// TODO: create handlers for routes
 			// Add, update and delete cart items
 			cartRoutes.POST("/add")
 			cartRoutes.PUT("/update")
 			cartRoutes.DELETE("/remove")
 
+			// TODO: create handlers for routes
 			// Empty cart
 			cartRoutes.POST("/cart/empty")
 
+			// TODO: create handlers for routes
 			// Checkout
 			cartRoutes.POST("cart/checkout")
 		}
 
 		orderRoutes := v1Routes.Group("/orders")
 		{
+			// TODO: create handlers for routes
 			// Create order
 			orderRoutes.POST("/")
 
+			// TODO: create handlers for routes
 			// Get all orders
 			orderRoutes.GET("/") // (Admin Only)
 
+			// TODO: create handlers for routes
 			// List all orders for user
 			orderRoutes.GET("/user/:id")
 
+			// TODO: create handlers for routes
 			// Update order status
 			orderRoutes.PUT("/:id/status")
 
+			// TODO: create handlers for routes
 			// Cancel an order
 			orderRoutes.PUT("/:id/cancel")
 
+			// TODO: create handlers for routes
 			// Return order
 			orderRoutes.POST("/:id/return")
 		}
