94e0e2db76ae1396c64318d440a67ab4642997cb
jxsoren <jxsoren@gmail.com>
2024-02-24 16:56:31 -0700

    un-commented product migration

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -13,9 +13,9 @@ func init() {
 
 func main() {
 
-	// if productsErr := initializers.DB.AutoMigrate(&models.Product{}); productsErr != nil {
-	// 	log.Fatalf("Error during migration: %v", productsErr)
-	// }
+	if productsErr := initializers.DB.AutoMigrate(&models.Product{}); productsErr != nil {
+		log.Fatalf("Error during migration: %v", productsErr)
+	}
 
 	if variantsErr := initializers.DB.AutoMigrate(&models.Variant{}); variantsErr != nil {
 		log.Fatalf("Error during Variant Table migration. Error: %v", variantsErr)
