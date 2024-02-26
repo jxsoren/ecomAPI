5b54225eccab3b1ce75826c995e7f132943a552b
jxsoren <jxsoren@gmail.com>
2024-02-25 18:57:54 -0700

    fixed error handling & removed mem addr pointer

--- utils/helpers.go
+++ utils/helpers.go
@@ -2,19 +2,22 @@ package helpers
 
 import (
 	"ecommerce_api/initializers"
+	"errors"
 	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func VerifyExistence(model interface{}, id string, c *gin.Context) {
 	notFoundMessage := fmt.Sprintf("Could not delete. No products found with ID of %s", id)
-	if result := initializers.DB.First(&model, id); result.Error.Error() == "record not found" {
-		c.JSON(http.StatusNotFound, gin.H{"error": notFoundMessage})
-		return
-	} else if result.Error != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+	if result := initializers.DB.First(model, id); result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			c.JSON(http.StatusNotFound, gin.H{"error": notFoundMessage})
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+		}
 		return
 	}
 }
