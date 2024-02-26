e4ad708a7faa339aa0231c54f8754b1a0c884544
jxsoren <jxsoren@gmail.com>
2024-02-25 18:58:58 -0700

    + comment

--- utils/helpers.go
+++ utils/helpers.go
@@ -13,6 +13,7 @@ import (
 func VerifyExistence(model interface{}, id string, c *gin.Context) {
 	notFoundMessage := fmt.Sprintf("Could not delete. No products found with ID of %s", id)
 	if result := initializers.DB.First(model, id); result.Error != nil {
+		// Search for record not found err in error tree
 		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
 			c.JSON(http.StatusNotFound, gin.H{"error": notFoundMessage})
 		} else {
