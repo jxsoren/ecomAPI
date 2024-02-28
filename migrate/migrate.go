0bfc1c4f14d41d7a3623a2143558db63d0f09326
jxsoren <jxsoren@gmail.com>
2024-02-27 23:20:48 -0700

    updated migration script

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -3,6 +3,7 @@ package main
 import (
 	"ecommerce_api/initializers"
 	"ecommerce_api/models"
+	"fmt"
 	"log"
 )
 
@@ -15,17 +16,30 @@ func main() {
 
 	if productsErr := initializers.DB.AutoMigrate(&models.Product{}); productsErr != nil {
 		log.Fatalf("Error during migration: %v", productsErr)
+		return
 	}
+
+	fmt.Println("Procucts migrated successfully!")
 
 	if variantsErr := initializers.DB.AutoMigrate(&models.Variant{}); variantsErr != nil {
 		log.Fatalf("Error during Variant Table migration. Error: %v", variantsErr)
+		return
 	}
+
+	fmt.Println("Variants migrated successfully!")
 
 	if reviewsErr := initializers.DB.AutoMigrate(&models.Review{}); reviewsErr != nil {
 		log.Fatalf("Error during Reviews Table Migration. Error: %v", reviewsErr)
+		return
 	}
+
+	fmt.Println("Reviews migrated successfully!")
 
 	if offersErr := initializers.DB.AutoMigrate(&models.Offer{}); offersErr != nil {
 		log.Fatalf("Error during Reviews Table Migration. Error: %v", offersErr)
+		return
 	}
+
+	fmt.Println("Offers migrated successfully!")
+
 }
